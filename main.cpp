415f2bf6856a2617b51dd7351f14fe8252252607
wendyji131 <31259905+wendyji131@users.noreply.github.com>
2018-03-04 11:42:11 -0800

    it's the main function
    the test file name must be the format: filename.txt

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "lexer.h"
+#include <fstream>
+#include <iomanip>  //setw
+using namespace std;
+
+void readFile(const string& filename);
+void printSourceCode(const string& filename);
+
+int main() {
+	string name;
+	
+	//test three sample source code
+	cout << "Enter test file name(format: XXXX.txt): ";
+	cin >> name;
+	cout << "Source Code:" << endl <<endl;
+	printSourceCode(name);
+	cout << "\nOutput:\n\n";
+	cout << left << setw(10) << "Token" << "\t" << "Lexeme" << endl << endl;
+	readFile(name);	
+
+	system("pause");
+	return 0;
+}
+
+void readFile(const string& filename) {
+	ifstream file(filename);
+	string word;
+
+	if (file.is_open()) {
+		while (file >> word) {
+			lexer(word);
+		}
+	}
+	else
+		cout << "Can't open the file: " << filename << endl;
+
+	file.close();
+	file.clear();
+	return;
+}
+
+void printSourceCode(const string& filename) {
+	ifstream file(filename);
+	string line;
+
+	if (file.is_open()) {
+		while (!file.eof()) {
+			getline(file, line);
+			cout << line << endl;
+		}
+	}
+	else
+		cout << "Can't open the file: " << filename << endl;
+	file.close();
+	file.clear();
+	cout << endl;
+	return;
+}
