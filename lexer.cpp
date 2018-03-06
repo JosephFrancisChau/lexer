df06c084826f7483467635e4d35ef126b53119ca
wendyji131 <31259905+wendyji131@users.noreply.github.com>
2018-03-05 23:22:15 -0800

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,20 +1,23 @@
-#include "lexer.h"
+#include "stdafx.h"
+//#include "lexer.h"
 #include <iostream>
 #include <map>
 #include <set>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <stack>
 #include <iomanip>  //setw
-
+#include <vector>
 using namespace std;
 
 map<string, string> tokens;
 set<string> operators = { "=","==","^=",">","<","=>","<=","+","-","*","/" };
-set<string> separators = { "%%", "[", "]", ";",",", "{","}","(",")","!" };
+set<string> separators = { "%%", "[", "]", ";",",", "{","}","(",")","!",":" };
 set<string> keywords = { "function", "int", "boolean",
 "real", "if", "endif", "else", "return", "put", "get", "while", "true", "false" };
 stack<string> comments;
+map<size_t, size_t> positions;	//store all the separators and operators address and length
 
 string type, value;
 bool isToken = false;
@@ -27,14 +30,83 @@ bool isID = false;
 
 void lexer(string& word);
 void printToken(string& type, string& value);
-void helper(string word1, string word2);
+
+ofstream coutfile;
+
+void readFile(const string& filename);
+void printSourceCode(const string& filename);
+void testSeparator(string& word);
+void splitWord(string& word);
+bool isSepOrOpe(string& word);
+int main() {
+	string name, name2;
+
+	//test sample source code
+	cout << "Enter test file name(format: filename.txt): ";
+	cin >> name;
+
+	cout << "Enter test output file name(format: filename.txt): ";
+	cin >> name2;
+
+	coutfile.open(name2);
+	cout << "source Code:" << endl << endl;
+	coutfile << "Source Code:" << endl << endl;
+	printSourceCode(name);
+
+	cout << "\nOutput:\n\n";
+	coutfile << "\nOutput:\n\n";
+	cout << left << setw(10) << "Token" << "\t" << "Lexeme" << endl << endl;
+	coutfile << left << setw(10) << "Token" << "\t" << "Lexeme" << endl << endl;
+	readFile(name);
+	coutfile.close();
+	system("pause");
+	return 0;
+}
+
+void readFile(const string& filename) {
+	ifstream file(filename);
+	string word;
+
+	if (file.is_open()) {
+		while (file >> word) {
+			testSeparator(word);
+		}
+	}
+	else
+		cout << "Can't open the file: " << filename << endl;
+
+	file.close();
+	file.clear();
+	return;
+}
+
+void printSourceCode(const string& filename) {
+	ifstream file(filename);
+	string line;
+
+	if (file.is_open()) {
+		while (!file.eof()) {
+			getline(file, line);
+			cout << line << endl;
+			coutfile << line << endl;
+		}
+	}
+	else
+		cout << "Can't open the file: " << filename << endl;
+	file.close();
+	file.clear();
+	cout << endl;
+	//coutfile << endl;
+	return;
+}
+
 
 void lexer(string& word) {
 	string temp1, temp2, value, type;
 	size_t len = word.length();
 	set<string>::iterator it;
 	if (!comments.empty()) {
-		if (word != "!" ) {
+		if (word != "!") {
 			comments.push(word);
 			return;
 		}
@@ -52,57 +124,21 @@ void lexer(string& word) {
 	}
 
 	if (!word.empty() && comments.empty()) {
-		if ((word[0] == '(') || (word[0] == '[') || (word[0] == '{') || word[0] == '!') {
+		it = separators.find(word);
+		//check is separator
+		if (it != separators.end()) {
 			isToken = true;
 			isSeparator = true;
-			value = word[0];
+			value = word;
 			type = "separator";
 			tokens.insert(pair<string, string>(type, value));
 			printToken(type, value);
-			if (word[0] == '!' || word == "!") {
+			if (word == "!") {
 				comments.push("!");
-			}
-			if (len > 1) {
-				word = word.erase(0, 1);
-				return lexer(word);
 			}
 			return;
 		}
-
-		//check first two char
-		if (word.substr(0, 2) == "%%") {
-			isSeparator = true;
-			isToken = true;
-			value = "%%";
-			type = "separator";
-			tokens.insert(pair<string, string>(type, value));
-			printToken(type, value);
-			word = word.erase(0, 2);
-			if (!word.empty()) {
-				return lexer(word);
-			}
-			else
-				return;
-		}
-				
-		//check the last char		
-		if ( word[len - 1] == ')' || word[len - 1] == ']' || word[len - 1] == '}' || word[len - 1] == '!') {
-			if (len == 1) {
-				isSeparator = true;
-				isToken = true;
-				value = word;
-				type = "separator";
-				tokens.insert(pair<string, string>(type, value));
-				printToken(type, value);
-				return;
-			}
-			else {
-				temp1 = word.substr(0, len - 1);
-				temp2 = word.substr(len - 1, 1);
-				helper(temp1, temp2);
-			}
-		}
-
+		
 		//check is operator
 		it = operators.find(word);
 		if (it != operators.end()) {
@@ -158,14 +194,24 @@ void lexer(string& word) {
 				return;
 			}
 		}
-		
+
 		//check integer and real
 		if (word[0] >= '0' && word[0] <= '9') {
 			//check is integer;
-			bool b;
-			for (size_t i = 0; i <= len; ++i) {
-				b = (word[i] >= '0'&&word[i] <= '9');
-				isInteger = b && true;
+			if (len == 1) {
+				isToken = true;
+				isInteger = true;
+				value = word;
+				type = "integer";
+				tokens.insert(pair<string, string>(type, value));
+				printToken(type, value);
+				return;
+			}
+			isInteger = true;
+			bool isDigit;
+			for (size_t i = 1; i <= len; ++i) {
+				isDigit = (word[i] >= '0'&&word[i] <= '9');
+				isInteger = isDigit && isInteger;
 			}
 			if (isInteger == true) {
 				isToken = true;
@@ -213,11 +259,93 @@ void lexer(string& word) {
 
 void printToken(string& type, string& value) {
 	cout << left << setw(10) << type << "\t" << value << endl;
+	
+	//ofstream coutfile;
+	//coutfile.open("result.txt");
+	//coutfile << "\nOutput:\n\n";
+	coutfile << left << setw(10) << type << "\t" << value << endl;
+	//coutfile.close();
+	
 }
 
-void helper(string word1, string word2) {
-	lexer(word1);
-	word1.clear();
-	lexer(word2);
-	word2.clear();
+void testSeparator(string& word) {
+	if (!word.empty()) {
+		if (isSepOrOpe(word)) {
+			splitWord(word);
+		}
+		else {
+			lexer(word);
+		}
+	}
+	return;
+}
+
+void splitWord(string& word) {
+	map<size_t, size_t>::iterator p;
+	string sub1, sub2, sub3;
+	vector<string> cleanwords;
+	size_t start = 0;
+	set<string>::iterator it1;
+	set<string>::iterator it2;
+	size_t len, firstP;
+	bool isSep = false;
+	bool isOpe = false;
+	
+	while (isSepOrOpe(word)) {
+		// find the first position of the sep or ope
+		p = positions.begin();
+		firstP = p->first;
+		len = p->second;
+		
+		//split word to two parts, because once there is a sep or ope at least can be split to two parts
+		if (firstP == 0) {
+			sub1 = word.substr(0, len);
+			sub2 = word.substr(len);
+		}
+		else if (firstP == word.length() - 1) {
+			sub1 = word.substr(0, firstP);
+			sub2 = word.substr(firstP);
+		}
+		else {
+			sub1 = word.substr(0, firstP);
+			sub2 = word.substr(firstP);
+		}
+		
+		cleanwords.push_back(sub1);
+		
+		word = sub2;
+	}
+	for (unsigned i = 0; i < cleanwords.size(); ++i) {
+		lexer(cleanwords.at(i));
+	}
+	
+	return;
+}
+
+bool isSepOrOpe(string& word) {
+	positions.clear();
+	bool exist = false;
+	set<string>::iterator it1;
+	set<string>::iterator it2;
+	size_t len = word.length(), iSep, iOpe;
+	bool isSep = false;
+	bool isOpe = false;
+	if (!word.empty()) {
+		for (it1 = separators.begin(); it1 != separators.end(); ++it1) {
+			if (word.find(*it1) != string::npos) {
+				isSep = true;
+				iSep = word.find(*it1);
+				positions.insert(pair<size_t, size_t>(iSep, (*it1).length()));
+			}
+		}
+		for (it2 = operators.begin(); it2 != operators.end(); ++it2) {
+			if (word.find(*it2) != string::npos) {
+				isOpe = true;
+				iOpe = word.find(*it2);
+				positions.insert(pair<size_t, size_t>(iOpe, (*it2).length()));
+			}
+		}
+		exist = isSep || isOpe;
+	}
+	return exist;
 }
